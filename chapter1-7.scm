a6e2562fbd75327982f2add39f223b9c6774ab57
Jed Northridge <northridge@gmail.com>
2009-11-28 17:01:51 -0500

    Minor cleanup

--- chapter1-7.scm
+++ chapter1-7.scm
@@ -1,10 +1,13 @@
 ;; The first three lines of this file were inserted by DrScheme. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname lilschemer) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ())))
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname chapter1-7) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ())))
 ;; The Little Schemer
 ;;
-;; Reading the book, following along with emacs, mzscheme, and Ubuntu.
+;; Reading the book, following along with emacs, drscheme, and Ubuntu.
 
+;;
+;; If in emacs,
+;;
 ;; run-scheme
 ;; C-x C-e Send last sexpr to Scheme
 ;; C-c C-r Send region
@@ -33,8 +36,6 @@
       ((null? l) #t)
       ((atom? (car l)) (lat? (cdr l)))
       (else #f))))
-;;(lat? (quote (a b c)))
-;;(lat? (quote ((a b) 1 2)))
 
 ;;
 ;; (member? a lat)
@@ -50,10 +51,6 @@
       ((equal? a (car lat)) #t)
       (else
        (member? a (cdr lat))))))
-;;(member? 'a '(a b c))
-;;(member? 'b '(a b c))
-;;(member? 'c '(a b c))
-;;(member? 'd '(a b c))
 
 ;;
 ;; (rember a lat)
@@ -70,10 +67,6 @@
       ((equal? a (car lat)) (cdr lat))
       (else
        (cons (car lat) (rember a (cdr lat)))))))
-;;(rember 'a '(a b c))
-;;(rember 'b '(a b c))
-;;(rember 'c '(a b c))
-;;(rember 'd '(a b c))
 
 ;;
 ;; (firsts lol)
@@ -108,7 +101,7 @@
       ((equal? (car lat) old) (cons old (cons new (cdr lat))))
       (else
        (cons (car lat) (insertR new old (cdr lat)))))))
-;;(insertR (quote jalapeno) (quote and) (quote (tacos tamales and salsa)))
+
 
 ;;
 ;; (insertL new old lat)
@@ -126,8 +119,6 @@
       ((equal? (car lat) old) (cons new lat))
       (else
        (cons (car lat) (insertL new old (cdr lat)))))))
-;;(insertL (quote a) (quote b) (quote (b c d)))
-;;(insertL (quote a) (quote d) (quote (b c d)))
 
 ;;
 ;; (subst new old lat)
@@ -145,8 +136,6 @@
       ((equal? old (car lat)) (cons new (cdr lat)))
       (else
        (cons (car lat) (subst new old (cdr lat)))))))
-;;(subst (quote new) (quote old) (quote (a b c old)))
-;;(subst (quote new) (quote old) (quote (old a b c)))
 
 ;;
 ;; (subst2 new o1 o2 lat)
@@ -166,7 +155,6 @@
       ((or (equal? (car lat) o1) (equal? (car lat) o2)) (cons new (cdr lat)))
       (else
        (cons (car lat) (subst2 new o1 o2 (cdr lat)))))))
-;;(subst2 (quote clean) (quote g1) (quote g2) (quote (a b c g2)))
 
 ;; multirember
 ;; Remove all occurrences of a within a list of atoms, lat.
@@ -179,10 +167,6 @@
       (else
        (cons (car lat) (multirember a (cdr lat)))))))
 
-;;(multirember (quote a) (quote (a b c)))
-;;(multirember (quote c) (quote (a b c)))
-;;(multirember (quote a) (quote (a b c a)))
-
 ;; multiinsertR
 (define multiinsertR
   (lambda (new old lat)
@@ -191,7 +175,6 @@
       ((equal? (car lat) old) (cons old (cons new (multiinsertR new old (cdr lat)))))
       (else
        (cons (car lat) (multiinsertR new old (cdr lat)))))))
-;;(multiinsertR (quote z) (quote a) (quote (a a a a)))
 
 ;; multiinsertL
 (define multiinsertL
@@ -201,7 +184,6 @@
       ((equal? (car lat) old) (cons new (multiinsertR new old (cdr lat))))
       (else
        (cons (car lat) (multiinsertL new old (cdr lat)))))))
-;;(multiinsertL (quote z) (quote a) (quote (a a a a)))
 
 ;; multisubst
 (define multisubst
@@ -211,10 +193,6 @@
       ((equal? (car lat) old) (cons new (multisubst new old (cdr lat))))
       (else
        (cons (car lat) (multisubst new old (cdr lat)))))))
-
-;;(multisubst (quote a) (quote b) (quote (a b c d)))
-;;(multisubst (quote a) (quote b) (quote (a a a a)))
-
 
 ;; o+
 ;; Add two numbers together
@@ -226,9 +204,6 @@
       (else
        (add1 (o+ n (sub1 m)))))))
 
-;;(o+ 1 1)
-;;(o+ 5 4)
-
 ;; Subtract m from n
 (define o-
   (lambda (n m)
@@ -237,9 +212,6 @@
       (else
        (sub1 (o- n (sub1 m)))))))
 
-;;(o- 10 5)
-;;(o- 10 1)
-
 ;; Add all of the numbers within tup
 (define addtup
   (lambda (tup)
@@ -247,8 +219,6 @@
       ((null? tup) 0)
       (else
        (o+ (car tup) (addtup (cdr tup)))))))
-;;(addtup (quote (1 2 3)))
-;;(addtup (quote (10 15 5)))
 
 ;; Multipy n x m
 (define ox
@@ -257,11 +227,6 @@
       ((zero? m) 0)
       (else
        (o+ n (ox n (sub1 m)))))))
-
-;;(ox 6 0)
-;;(ox 6 1)
-;;(ox 6 3)
-;;(ox 6 10)
 
 ;; Given two tuples of equal length, generate a new tuple containing each
 ;; pair of elements added together
@@ -273,9 +238,6 @@
       (else
        (cons (o+ (car tup1) (car tup2)) (tup+ (cdr tup1) (cdr tup2)))))))
 
-;;(tup+ (quote (1 2)) (quote (3 5)))
-;;(tup+ (quote (5 5 5 5 56)) (quote (4 27 8 9)))
-
 ;; greater than
 ;; p72
 (define o>
@@ -286,10 +248,6 @@
       (else
        (o> (sub1 n) (sub1 m))))))
 
-;;(o> 3 2)
-;;(o> 2 3)
-;;(o> 2 2)
-
 ;; less than
 ;; p 73
 (define o<
@@ -299,9 +257,6 @@
       ((zero? n) #t)
       (else
        (o< (sub1 n) (sub1 m))))))
-;;(o< 3 2)
-;;(o< 2 3)
-;;(o< 2 2)
 
 ;; numeric equality
 ;; p 74
@@ -313,9 +268,6 @@
       (else
        #t))))
 
-;;(o= 1 2)
-;;(o= 2 2)
-
 ;; exponentiation n^m
 (define o^
   (lambda (n m)
@@ -323,10 +275,6 @@
       ((zero? m) 1)
       (else
        (ox n (o^ n (sub1 m)))))))
-
-;;(o^ 2 0)
-;;(o^ 2 3)
-;;(o^ 2 5)
 
 ;; length
 ;; returns the number of elements in a lat
@@ -337,7 +285,6 @@
       ((null? lat) 0)
       (else
        (add1 (olength (cdr lat)))))))
-;;(olength (quote(a b c)))
 
 ;; (pick n lat)
 ;;
@@ -353,9 +300,6 @@
      (else
       (pick (sub1 n) (cdr lat))))))
 
-;;(pick 3 (quote (a b c)))
-;;(pick 1 (quote (a b c)))
-
 ;; (rempick n lat)
 ;;
 ;; Given a number, n, and a list of atoms, lat, remove the nth item in
@@ -369,11 +313,6 @@
      ((zero? (sub1 n)) (cdr lat))
      (else
       (cons (car lat) (rempick (sub1 n) (cdr lat)))))))
-
-;;(rempick 1 (quote (a b c d)))
-;;(rempick 2 (quote (a b c d)))
-;;(rempick 3 (quote (a b c d)))
-;;(rempick 4 (quote (a b c d)))
 
 ;; (no-nums lat)
 ;;
@@ -390,10 +329,6 @@
      (else
       (cons (car lat) (no-nums (cdr lat)))))))
 
-;;(no-nums (quote (a 1 b 2 c 3 d 4 e 5)))
-;;(no-nums (quote (a b c d e)))
-;;(no-nums (quote (1 2 3 4 5)))
-
 ;; (all-nums lat)
 ;;
 ;; Given a list of atoms, lat, return a tuple containing all of the
@@ -408,10 +343,6 @@
      ((number? (car lat)) (cons (car lat) (all-nums (cdr lat))))
      (else
       (all-nums (cdr lat))))))
-
-;;(all-nums (quote (a 1 b 2 c 3 d 4 e 5)))
-;;(all-nums (quote (a b c d e)))
-;;(all-nums (quote (1 2 3 4 5)))
 
 ;; (eqan? a1 a2)
 ;;
@@ -431,12 +362,6 @@
      (else
       (eq? a1 a2)))))
 
-;;(eqan? 1 1)
-;;(eqan? 1 2)
-;;(eqan? (quote a) (quote a))
-;;(eqan? (quote a) (quote b))
-;;(eqan? (quote a) 1)
-
 ;; (occur a lat)
 ;;
 ;; Given an atom, a, and a list of atoms, lat, return the number of
@@ -451,9 +376,6 @@
      ((eqan? a (car lat)) (add1 (occur a (cdr lat))))
      (else
       (occur a (cdr lat))))))
-
-;;(occur 1 (quote (a 1 b 1 b 1 c)
-;;(occur (quote b) (quote (a 1 b 1 b 1 c)))
 
 ;; (one? n)
 ;;
@@ -470,10 +392,6 @@
      ((zero? (sub1 n)) #t)
      (else #f))))
 
-;;(one? 0)
-;;(one? 1)
-;;(one? 2)
-
 ;; (rempick-again n lat)
 ;;
 ;; rempick-again, but using one?
@@ -487,11 +405,6 @@
      ((one? n) (cdr lat))
      (else
       (cons (car lat) (rempick-again (sub1 n) (cdr lat)))))))
-
-;;(rempick-again 1 (quote (a b c d)))
-;;(rempick-again 2 (quote (a b c d)))
-;;(rempick-again 3 (quote (a b c d)))
-;;(rempick-again 4 (quote (a b c d)))
 
 ;; (rember* a l)
 ;;Chapter 5, p. 81
@@ -507,11 +420,6 @@
      (else
       (cons (rember* a (car l)) (rember* a (cdr l)))))))
 
-;;(rember* (quote a) (quote ((a b) (a b))))
-;;(rember* (quote a) (quote ((a b))))
-;;(rember* (quote a) (quote (a b c (a b))))
-;;(rember* (quote a) (quote (b a b)))
-
 ;; (insertR* new old l)
 ;; Chapter 5, p. 82
 (define insertR*
@@ -526,12 +434,6 @@
         (cons (car l) (insertR* new old (cdr l))))))
      (else
       (cons (insertR* new old (car l)) (insertR* new old (cdr l)))))))
-;;(insertR* (quote new) (quote old) (quote (a b)))
-;;(insertR* (quote new) (quote old) (quote (a b old)))
-;;(insertR* (quote new) (quote old) (quote (old a old b old)))
-;;(insertR* (quote new) (quote old) (quote ((old a b))))
-;;(insertR* (quote new) (quote old) (quote (a b c (old a b))))
-;;(insertR* (quote new) (quote old) (quote (old b (a old) b (a old b))))
 
 ;; (occur* a l)
 ;; Chapter 5, p. 85
@@ -546,10 +448,6 @@
         (occur* a (cdr l)))))
       (else
        (o+ (occur* a (car l)) (occur* a (cdr l)))))))
-;;(occur* (quote a) (quote ()))
-;;(occur* (quote a) (quote (a b c d)))
-;;(occur* (quote a) (quote (a b a c d)))
-;;(occur* (quote a) (quote (a b a (c a (a a) d))))
 
 ;; (subst* new old l)
 ;; Chapter 5, p. 85
@@ -564,11 +462,46 @@
         (cons (car l) (subst* new old (cdr l))))))
      (else
       (cons (subst* new old (car l)) (subst* new old (cdr l)))))))
+
+;;(rember* (quote a) (quote ((a b) (a b))))
+;;(rember* (quote a) (quote ((a b))))
+;;(rember* (quote a) (quote (a b c (a b))))
+;;(rember* (quote a) (quote (b a b)))
+
+;;(insertR* (quote new) (quote old) (quote (a b)))
+;;(insertR* (quote new) (quote old) (quote (a b old)))
+;;(insertR* (quote new) (quote old) (quote (old a old b old)))
+;;(insertR* (quote new) (quote old) (quote ((old a b))))
+;;(insertR* (quote new) (quote old) (quote (a b c (old a b))))
+;;(insertR* (quote new) (quote old) (quote (old b (a old) b (a old b))))
+
+;;(occur* (quote a) (quote ()))
+;;(occur* (quote a) (quote (a b c d)))
+;;(occur* (quote a) (quote (a b a c d)))
+;;(occur* (quote a) (quote (a b a (c a (a a) d))))
+
 ;;(subst* (quote new) (quote old) (quote ()))
 ;;(subst* (quote new) (quote old) (quote (a old)))
 ;;(subst* (quote new) (quote old) (quote (a old (old))))
 ;;(subst* (quote new) (quote old) (quote (a b old (c old (a old) old))))
-       
+
+;;(insertL* (quote new) (quote old) (quote (a b)))
+;;(insertL* (quote new) (quote old) (quote (a b old)))
+;;(insertL* (quote new) (quote old) (quote (old a old b old)))
+;;(insertL* (quote new) (quote old) (quote ((old a b))))
+;;(insertL* (quote new) (quote old) (quote (a b c (old a b))))
+;;(insertL* (quote new) (quote old) (quote (old b (a old) b (a old b))))
+
+;;(member* (quote c) (quote (a b)))
+;;(member* (quote c) (quote (a b c)))
+;;(member* (quote c) (quote (c a c b c)))
+;;(member* (quote c) (quote ((c a b))))
+;;(member* (quote c) (quote (a b d (c a b))))
+;;(member* (quote c) (quote (b (a b) b (a b (d)) c)))
+
+;;(leftmost (quote (a b)))
+;;(leftmost (quote ((a) b)))
+
 ;; (insertL* new old l)
 ;; Chapter 5, p. 86
 (define insertL*
@@ -583,12 +516,6 @@
         (cons (car l) (insertL* new old (cdr l))))))
      (else
       (cons (insertL* new old (car l)) (insertL* new old (cdr l)))))))
-;;(insertL* (quote new) (quote old) (quote (a b)))
-;;(insertL* (quote new) (quote old) (quote (a b old)))
-;;(insertL* (quote new) (quote old) (quote (old a old b old)))
-;;(insertL* (quote new) (quote old) (quote ((old a b))))
-;;(insertL* (quote new) (quote old) (quote (a b c (old a b))))
-;;(insertL* (quote new) (quote old) (quote (old b (a old) b (a old b))))
 
 ;; (member* a l)
 ;; Chapter 5, p. 87
@@ -600,12 +527,6 @@
        (or (eqan? a (car l)) (member* a (cdr l))))
      (else
       (or (member* a (car l)) (member* a (cdr l)))))))
-;;(member* (quote c) (quote (a b)))
-;;(member* (quote c) (quote (a b c)))
-;;(member* (quote c) (quote (c a c b c)))
-;;(member* (quote c) (quote ((c a b))))
-;;(member* (quote c) (quote (a b d (c a b))))
-;;(member* (quote c) (quote (b (a b) b (a b (d)) c)))
 
 ;; (leftmost* l)
 ;;
@@ -617,8 +538,6 @@
       ((atom? (car l)) (car l))
       (else
        (leftmost (car l))))))
-;;(leftmost (quote (a b)))
-;;(leftmost (quote ((a) b)))
 
 ;; (eqlist? l1 l2)
 ;; Chapter 5, p. 92
@@ -632,13 +551,6 @@
         (equal? (car l1) (car l2))
         (eqlist? (cdr l1) (cdr l2)))))))
 
-;;(eqlist? (quote ()) (quote ()))
-;;(eqlist? (quote ()) (quote (a)))
-;;(eqlist? (quote (a)) (quote (a)))
-;;(eqlist? (quote (a)) (quote (b)))
-;;(eqlist? (quote (a b)) (quote (a b)))
-;;(eqlist? (quote (a (b))) (quote (a (c))))
-
 ;; (equal? s1 s2)
 ;; Chapter 5, p. 92
 (define equal
@@ -648,14 +560,6 @@
       ((or (atom? s1) (atom? s2)) #f)
       (else
        (eqlist? s1 s2)))))
-;;(equal (quote ()) (quote ()))
-;;(equal (quote a) (quote a))
-;;(equal (quote a) (quote b))
-;;(equal (quote ()) (quote a))
-;;(equal (quote (a b)) (quote (a b)))
-;;(equal (quote (a b)) (quote (a b c)))
-;;(equal (quote (a b (c d) (e f) g)) (quote (a b (c d) (e f) g)))
-;;(equal (quote (a b (c d) (e f (r r)) () g)) (quote (a b (c d) (e f (r r)) () g)))
 
 ;; (rember s l)
 ;; Chapter 5, p. 94
@@ -686,31 +590,19 @@
        (and
         (numbered? (car aexp))
         (numbered? (car (cdr (cdr aexp)))))))))
-;;(numbered? (quote 4))
-;;(numbered? (quote (4 + 5)))
-;;(numbered? (quote ((3 + 1) + 5)))
-;;(numbered? (quote ((3 ^ 2) + (5 - 2))))
 
 ;; Return the first subexpression of a nexp
 (define 1st-sub
   (lambda (aexp)
     (car aexp)))
-;;(1st-sub (quote (4 + 5)))
-;;(1st-sub (quote ((4 ^ 3) + 5)))
 
 (define 2nd-sub
   (lambda (aexp)
     (car (cdr (cdr aexp)))))
-;;(2nd-sub (quote (4 + 5)))
-;;(2nd-sub (quote ((4 ^ 3) + 5)))
-;;(2nd-sub (quote ((4 ^ 3) + (5 - 2))))
 
 (define operator
   (lambda (aexp)
     (car (cdr aexp))))
-;;(operator (quote (4 + 5)))
-;;(operator (quote ((4 ^ 3) ^ 5)))
-;;(operator (quote ((4 ^ 3) - (5 - 2))))
 
 ;; (value aexp)
 ;; Chapter 6, p. 102
@@ -722,12 +614,6 @@
       ((equal? (operator aexp) (quote -)) (o- (value (1st-sub aexp)) (value (2nd-sub aexp))))
       (else
        (o^ (value (1st-sub aexp)) (value (2nd-sub aexp)))))))
-;;(value (quote 1))
-;;(value (quote (1 + 3)))
-;;(value (quote (3 - 1)))
-;;(value (quote (2 ^ 3)))
-;;(value (quote (1 + (3 ^ 4))))
-;;(value (quote ((1 + (3 ^ 4)) - 2)))
 
 ;; (set? lat)
 ;; Chapter 7, pg. 111
@@ -740,11 +626,6 @@
       ((member? (car lat) (cdr lat)) #f)
       (else
        (set? (cdr lat))))))
-;;(set? (quote ()))
-;;(set? (quote (a)))
-;;(set? (quote (a a)))
-;;(set? (quote (a b c)))
-;;(set? (quote (a b c d e f g h i j k f)))
 
 ;; (makeset lat)
 ;; Ch 7, p 112
@@ -757,12 +638,6 @@
       ((member? (car lat) (cdr lat)) (makeset (cdr lat)))
       (else
        (cons (car lat) (makeset (cdr lat)))))))
-;;(makeset (quote ()))
-;;(makeset (quote (a)))
-;;(makeset (quote (a a)))
-;;(makeset (quote (a b c)))
-;;(makeset (quote (a b c d e f g h i j k f)))
-;;(makeset (quote (a b a b a b a b a)))
 
 ;; (subset? set1 set2)
 ;; Ch 7, p 113
@@ -775,10 +650,6 @@
       (else (and
        (member? (car set1) set2)
        (subset? (cdr set1) set2))))))
-;;(subset? (quote ()) (quote (a b)))
-;;(subset? (quote (a)) (quote (a)))
-;;(subset? (quote (a b c)) (quote (a b)))
-;;(subset? (quote (a b c d)) (quote (d a b c)))
 
 ;; (eqset? set1 set2)
 ;; Ch 7, p 114
@@ -789,10 +660,6 @@
     (and
      (subset? set1 set2)
      (subset? set2 set1))))
-;;(eqset? (quote ()) (quote (a b)))
-;;(eqset? (quote (a)) (quote (a)))
-;;(eqset? (quote (a b c)) (quote (a b)))
-;;(eqset? (quote (a b c d)) (quote (d a b c)))
 
 ;; (intersect? set1 set2)
 ;; Ch 7, p 115
@@ -806,10 +673,6 @@
        (or
         (member? (car set1) set2)
         (intersect? (cdr set1) set2))))))
-;;(intersect? (quote ()) (quote (a b)))
-;;(intersect? (quote (a)) (quote (a)))
-;;(intersect? (quote (a b c)) (quote (d e f)))
-;;(intersect? (quote (a b c d)) (quote (d e f g)))
 
 ;; (intersect set1 set)
 ;; Ch 7, p 116
@@ -822,10 +685,6 @@
       ((member? (car set1) set2) (cons (car set1) (intersect (cdr set1) set2)))
       (else
        (intersect (cdr set1) set2)))))
-;;(intersect (quote ()) (quote (a b)))
-;;(intersect (quote (a)) (quote (a)))
-;;(intersect (quote (a b c)) (quote (b d e f)))
-;;(intersect (quote (a b c d)) (quote (d c e f g)))
 
 ;; (union set1 set)
 ;; Ch 7, p 116
@@ -838,10 +697,6 @@
       ((member? (car set1) set2) (union (cdr set1) set2))
       (else
        (cons (car set1) (union (cdr set1) set2))))))
-;;(union (quote ()) (quote (a b)))
-;;(union (quote (a)) (quote (a)))
-;;(union (quote (a b c)) (quote (b d e f)))
-;;(union (quote (a b c d)) (quote (d c e f g)))
 
 ;; (intersectall l-set)
 ;; Ch 7, p 116
@@ -853,4 +708,3 @@
       ((null? (cdr l-set)) (car l-set))
       (else
        (intersect (car l-set) (intersect-all (cdr l-set)))))))
-(intersect-all (quote ((a b c) (a b c d e f) (b c f g))))
